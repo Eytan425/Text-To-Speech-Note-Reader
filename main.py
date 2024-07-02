1b8ffbbce6a06ab9753efd791b126aba6e6253cb
Eytan Benittah <eytanbenittah@gmail.com>
2024-07-02 10:44:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pyttsx3
+
+def read_text_from_file(file_path):
+    file = open(file_path, 'r')
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return None
+    
+def read_file_contents(file_content):
+        engine = pyttsx3.init()
+        engine.say(file_content)
+        engine.runAndWait()
+
+path = input("Enter file path: ")
+read_file_contents(read_text_from_file(path))
+     
