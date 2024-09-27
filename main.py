61838138867af5d04d3b4444f626a2e5c6e3d6eb
Eytan Benittah <eytanbenittah@gmail.com>
2024-09-27 16:14:21 +0300

    Tried something and it didn't work

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pyttsx3
 
+def add_to_file(path):
+    open_file = open(path, "a")
+    info = input("Enter what you would like to write: ")
+    open_file.write(info)
+    open_file.close()
+
 def read_text_from_file(file_path):
     file = open(file_path, 'r')
     try:
@@ -8,12 +14,17 @@ def read_text_from_file(file_path):
         return content
     except FileNotFoundError:
         return None
-    
+  
 def read_file_contents(file_content):
         engine = pyttsx3.init()
         engine.say(file_content)
         engine.runAndWait()
 
-path = input("Enter the path to the file: ")
-read_file_contents(read_text_from_file(path))
-     
+def Main():        
+    choice = input("Would you like to write in the file: ")
+    if(choice.lower() == 'yes'):
+        path = input("Enter the path to the file: ")
+        print(add_to_file(path))
+        read_text_from_file(path)
+
+print(Main())        
